7f63e16adaa1c78230637514dca0480990749ec1
FineFindus <FineFindus@proton.me>
2023-11-14 20:07:22 +0100

    feat(client): add job description methods

--- src/client.rs
+++ src/client.rs
@@ -1,10 +1,8 @@
-use zbus::{fdo::ObjectManagerProxy, Connection};
+use zbus::{fdo::ObjectManagerProxy, zvariant::OwnedObjectPath, Connection};
 
-use crate::manager;
+use crate::{job, manager};
 
 /// Utility routines for accessing the UDisks service
-///
-///
 pub struct Client {
     connection: Connection,
     object_manager: zbus::fdo::ObjectManagerProxy<'static>,
@@ -45,5 +43,53 @@ impl Client {
     /// Returns a reference to the manager interface.
     pub fn manager(&self) -> &manager::ManagerProxy<'_> {
         &self.manager
+    }
+
+    /// Gets a human-readable and localized text string describing the operation of job.
+    ///
+    /// For known job types, see the documentation for [`job::JobProxy::operation`].
+    pub fn job_description_from_operation(&self, operation: &str) -> String {
+        //TODO use gettext to translate the strings
+        match operation {
+            "ata-smart-selftest" => String::from("SMART self-test"),
+            "drive-eject" => String::from("Ejecting Medium"),
+            "encrypted-unlock" => String::from("Unlocking Device"),
+            "encrypted-lock" => String::from("Locking Device"),
+            "encrypted-modify" => String::from("Modifying Encrypted Device"),
+            "encrypted-resize" => String::from("Resizing Encrypted Device"),
+            "swapspace-start" => String::from("Starting Swap Device"),
+            "swapspace-stop" => String::from("Stopping Swap Device"),
+            "swapspace-modify" => String::from("Modifying Swap Device"),
+            "filesystem-check" => String::from("Checking Filesystem"),
+            "filesystem-mount" => String::from("Mounting Filesystem"),
+            "filesystem-unmount" => String::from("Unmounting Filesystem"),
+            "filesystem-modify" => String::from("Modifying Filesystem"),
+            "filesystem-repair" => String::from("Repairing Filesystem"),
+            "filesystem-resize" => String::from("Resizing Filesystem"),
+            "format-erase" => String::from("Erasing Device"),
+            "format-mkfs" => String::from("Creating Filesystem"),
+            "loop-setup" => String::from("Setting Up Loop Device"),
+            "partition-modify" => String::from("Modifying Partition"),
+            "partition-delete" => String::from("Deleting Partition"),
+            "partition-create" => String::from("Creating Partition"),
+            "cleanup" => String::from("Cleaning Up"),
+            "ata-secure-erase" => String::from("ATA Secure Erase"),
+            "ata-enhanced-secure-erase" => String::from("ATA Enhanced Secure Erase"),
+            "md-raid-stop" => String::from("Stopping RAID Array"),
+            "md-raid-start" => String::from("Starting RAID Array"),
+            "md-raid-fault-device" => String::from("Marking Device as Faulty"),
+            "md-raid-remove-device" => String::from("Removing Device from Array"),
+            "md-raid-add-device" => String::from("Adding Device to Array"),
+            "md-raid-set-bitmap" => String::from("Setting Write-Intent Bitmap"),
+            "md-raid-create" => String::from("Creating RAID Array"),
+            _ => format!("Unkown ({})", operation),
+        }
+    }
+
+    /// Gets a human-readable and localized text string describing the operation of job.
+    ///
+    /// For known job types, see the documentation for [`job::JobProxy::operation`].
+    pub async fn job_description(&self, job: job::JobProxy<'_>) -> zbus::Result<String> {
+        Ok(self.job_description_from_operation(&job.operation().await?))
     }
 }
