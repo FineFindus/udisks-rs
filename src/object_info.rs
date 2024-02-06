b38d08f234fdb1239c09a4d804c563e51348b4b4
FineFindus <FineFindus@proton.me>
2024-02-06 15:22:47 +0100

    feat(objectInfo): implement info_for_block

--- src/object_info.rs
+++ src/object_info.rs
@@ -52,7 +52,56 @@ impl ObjectInfo {
         block: block::BlockProxy<'_>,
         partition: Option<partition::PartitionProxy<'_>>,
     ) {
-        unimplemented!()
+        //TODO: use gettext
+        //https://github.com/storaged-project/udisks/blob/0b3879ab1d429b8312eaad0deb1b27e5545e39c1/udisks/udisksobjectinfo.c#L252
+        self.icon = Some(GIcon("drive-removable-media"));
+        self.icon_symbolic = Some(GIcon("drive-removable-media_symbolic"));
+        self.name = block
+            .preferred_device()
+            .await
+            .ok()
+            .and_then(|dev| String::from_utf8(dev).ok());
+
+        let size = block.size().await;
+        if let Ok(size) = size {
+            let size = client.size_for_display(size, false, false);
+            self.description = Some(format!("{} Block Device", size));
+        } else {
+            self.description = Some("Block Device".to_owned());
+        }
+
+        let mut partition_number = None;
+        if let Some(partition) = partition {
+            partition_number = partition.number().await.ok();
+
+            // Translators: Used to describe a partition of a block device.
+            //              The %u is the partition number.
+            //              The %s is the description for the block device (e.g. "5 GB Block Device").
+            self.description = Some(format!(
+                "Partition {} of {}",
+                partition_number.expect("Failed to read partition number"),
+                //Safe to unwrap, we have previously set this
+                self.description.as_ref().unwrap()
+            ));
+        }
+
+        // Translators: String used for one-liner description of a block device.
+        //              The first %s is the description of the object (e.g. "50 GB Block Device").
+        //              The second %s is the special device file (e.g. "/dev/sda2").
+        //TODO: C version calls preferred_device again, instead of using name, why?
+        self.one_liner = Some(format!(
+            "{} ({})",
+            self.description.as_ref().unwrap(),
+            self.name.as_ref().unwrap()
+        ));
+
+        self.sort_key = Some(format!(
+            "02_block_{}_{}",
+            // safe to unwrap, object apth always have at least one `/`
+            self.object.object_path().split('/').last().unwrap(),
+            //TODO: use asnyc closure when stable
+            partition_number.unwrap_or(0)
+        ))
     }
 
     pub(crate) fn info_for_drive(
