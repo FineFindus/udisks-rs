f2c44c1dc812375e2fbaac264d8ff7141904d04b
FineFindus <FineFindus@proton.me>
2024-02-11 15:16:47 +0100

    feat: add disk sizes example

---
+++ examples/disk_sizes.rs
@@ -0,0 +1,25 @@
+#[tokio::main]
+async fn main() -> zbus::Result<()> {
+    let client = udisks_rs::Client::new().await?;
+
+    for object in client
+        .object_manager()
+        .get_managed_objects()
+        .await?
+        .into_iter()
+        .filter_map(|(object_path, _)| client.object(object_path).ok())
+    {
+        //only use objects that have a drive
+        let Ok(drive) = object.drive().await else {
+            continue;
+        };
+
+        // print model and size
+        println!(
+            "{}: {}",
+            drive.model().await?,
+            client.size_for_display(drive.size().await?, false, false)
+        );
+    }
+    Ok(())
+}
